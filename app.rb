d797ca4b2d1a98773f11862cdf58962aa21d70fd
irhabano234 <irha.bano@gmail.com>
2018-06-26 14:19:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  gets '/'do
+    "Irha is cool!"
+  end 
+    
+    gets 'info/'do
+      "Irha is awesome!"
+    end 
+      
 end
